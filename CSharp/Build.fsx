8e3bc7cea5eb5f4db8011cc021d64c1a5a4a8018
Mark Seemann <mark@ploeh.dk>
2018-02-20 15:37:53 +0100

    Add build scripts

---
+++ CSharp/Build.fsx
@@ -0,0 +1,11 @@
+﻿#r @"packages/FAKE.4.64.5/tools/FakeLib.dll"
+
+open Fake
+
+Target "Build" <| fun _ ->
+    !! "**/UserManagement.sln"
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+
+
+RunTargetOrDefault "Build"
---
+++ CSharp/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+packages/FAKE.4.64.5/tools/FAKE.exe Build.fsx
