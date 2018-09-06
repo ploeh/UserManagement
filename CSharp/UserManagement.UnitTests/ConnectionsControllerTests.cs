d03f5af1414dd852a00dcb1748638ecf08015f21
Mark Seemann <mark@ploeh.dk>
2018-09-06 20:28:52 +0200

    Use Fake instead of Mocks and Stubs

--- CSharp/UserManagement.UnitTests/ConnectionsControllerTests.cs
+++ CSharp/UserManagement.UnitTests/ConnectionsControllerTests.cs
@@ -13,120 +13,92 @@ namespace Ploeh.Samples.UserManagement.UnitTests
     {
         [Theory, UserManagementTestConventions]
         public void UsersSuccessfullyConnect(
-            [Frozen]Mock<IUserReader> readerTD,
-            [Frozen]Mock<IUserRepository> repoTD,
+            [Frozen(Matching.ImplementedInterfaces)]FakeDB db,
             User user,
             User otherUser,
             ConnectionsController sut)
         {
-            readerTD
-                .Setup(r => r.Lookup(user.Id.ToString()))
-                .Returns(Result.Success<User, IUserLookupError>(user));
-            readerTD
-                .Setup(r => r.Lookup(otherUser.Id.ToString()))
-                .Returns(Result.Success<User, IUserLookupError>(otherUser));
+            db.Add(user);
+            db.Add(otherUser);
+            db.IsDirty = false;
 
             var actual = sut.Post(user.Id.ToString(), otherUser.Id.ToString());
 
             var ok = Assert.IsAssignableFrom<OkNegotiatedContentResult<User>>(
                 actual);
             Assert.Equal(otherUser, ok.Content);
-            repoTD.Verify(r => r.Update(user));
+            Assert.True(db.IsDirty);
             Assert.Contains(otherUser.Id, user.Connections);
         }
 
         [Theory, UserManagementTestConventions]
         public void UsersFailToConnectWhenUserIdIsInvalid(
-            [Frozen]Mock<IUserReader> readerTD,
-            [Frozen]Mock<IUserRepository> repoTD,
+            [Frozen(Matching.ImplementedInterfaces)]FakeDB db,
             string userId,
             User otherUser,
             ConnectionsController sut)
         {
             Assert.False(int.TryParse(userId, out var _));
-            readerTD
-                .Setup(r => r.Lookup(userId))
-                .Returns(Result.Error<User, IUserLookupError>(
-                    UserLookupError.InvalidId));
-            readerTD
-                .Setup(r => r.Lookup(otherUser.Id.ToString()))
-                .Returns(Result.Success<User, IUserLookupError>(otherUser));
+            db.Add(otherUser);
+            db.IsDirty = false;
 
             var actual = sut.Post(userId, otherUser.Id.ToString());
 
             var err = Assert.IsAssignableFrom<BadRequestErrorMessageResult>(actual);
             Assert.Equal("Invalid user ID.", err.Message);
-            repoTD.Verify(r => r.Update(It.IsAny<User>()), Times.Never());
+            Assert.False(db.IsDirty);
         }
 
         [Theory, UserManagementTestConventions]
         public void UsersFailToConnectWhenOtherUserIdIsInvalid(
-            [Frozen]Mock<IUserReader> readerTD,
-            [Frozen]Mock<IUserRepository> repoTD,
+            [Frozen(Matching.ImplementedInterfaces)]FakeDB db,
             User user,
             string otherUserId,
             ConnectionsController sut)
         {
             Assert.False(int.TryParse(otherUserId, out var _));
-            readerTD
-                .Setup(r => r.Lookup(user.Id.ToString()))
-                .Returns(Result.Success<User, IUserLookupError>(user));
-            readerTD
-                .Setup(r => r.Lookup(otherUserId))
-                .Returns(Result.Error<User, IUserLookupError>(
-                    UserLookupError.InvalidId));
+            db.Add(user);
+            db.IsDirty = false;
 
             var actual = sut.Post(user.Id.ToString(), otherUserId);
 
             var err = Assert.IsAssignableFrom<BadRequestErrorMessageResult>(actual);
             Assert.Equal("Invalid ID for other user.", err.Message);
-            repoTD.Verify(r => r.Update(It.IsAny<User>()), Times.Never());
+            Assert.False(db.IsDirty);
         }
 
         [Theory, UserManagementTestConventions]
         public void UsersDoNotConnectWhenUserDoesNotExist(
-            [Frozen]Mock<IUserReader> readerTD,
-            [Frozen]Mock<IUserRepository> repoTD,
-            string userId,
+            [Frozen(Matching.ImplementedInterfaces)]FakeDB db,
+            int userId,
             User otherUser,
             ConnectionsController sut)
         {
-            readerTD
-                .Setup(r => r.Lookup(userId))
-                .Returns(Result.Error<User, IUserLookupError>(
-                    UserLookupError.NotFound));
-            readerTD
-                .Setup(r => r.Lookup(otherUser.Id.ToString()))
-                .Returns(Result.Success<User, IUserLookupError>(otherUser));
+            db.Add(otherUser);
+            db.IsDirty = false;
 
-            var actual = sut.Post(userId, otherUser.Id.ToString());
+            var actual = sut.Post(userId.ToString(), otherUser.Id.ToString());
 
             var err = Assert.IsAssignableFrom<BadRequestErrorMessageResult>(actual);
             Assert.Equal("User not found.", err.Message);
-            repoTD.Verify(r => r.Update(It.IsAny<User>()), Times.Never());
+            Assert.False(db.IsDirty);
         }
 
         [Theory, UserManagementTestConventions]
         public void UsersDoNotConnectWhenOtherUserDoesNotExist(
-            [Frozen]Mock<IUserReader> readerTD,
-            [Frozen]Mock<IUserRepository> repoTD,
+            [Frozen(Matching.ImplementedInterfaces)]FakeDB db,
             User user,
             int otherUserId,
             ConnectionsController sut)
         {
-            readerTD
-                .Setup(r => r.Lookup(user.Id.ToString()))
-                .Returns(Result.Success<User, IUserLookupError>(user));
-            readerTD
-                .Setup(r => r.Lookup(otherUserId.ToString()))
-                .Returns(Result.Error<User, IUserLookupError>(
-                    UserLookupError.NotFound));
+            db.Add(user);
+            db.IsDirty = false;
 
             var actual = sut.Post(user.Id.ToString(), otherUserId.ToString());
 
             var err = Assert.IsAssignableFrom<BadRequestErrorMessageResult>(actual);
             Assert.Equal("Other user not found.", err.Message);
-            repoTD.Verify(r => r.Update(It.IsAny<User>()), Times.Never());
+            Assert.False(db.IsDirty);
         }
     }
 }
---
+++ CSharp/UserManagement.UnitTests/FakeDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.UserManagement.UnitTests
+{
+    public class FakeDB : Collection<User>, IUserReader, IUserRepository
+    {
+        public IResult<User, IUserLookupError> Lookup(string id)
+        {
+            if (!(int.TryParse(id, out int i)))
+                return Result.Error<User, IUserLookupError>(
+                    UserLookupError.InvalidId);
+
+            var user = this.FirstOrDefault(u => u.Id == i);
+            if (user == null)
+                return Result.Error<User, IUserLookupError>(
+                    UserLookupError.NotFound);
+
+            return Result.Success<User, IUserLookupError>(user);
+        }
+
+        public bool IsDirty { get; set; }
+
+        public void Update(User user)
+        {
+            IsDirty = true;
+            if (!Contains(user))
+                Add(user);
+        }
+    }
+}
